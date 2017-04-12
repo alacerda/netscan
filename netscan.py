3ab2c2f03f0caa60067d68874cae60da2e315cac
root <root@localhost.localdomain>
2017-04-11 22:40:08 -0300

    initial commit

---
+++ netscan.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import sys
+import socket
+
+alvo = sys.argv[1]
+portas = sys.argv[2]
+
+class Sock:
+	def __init__(self, target, port):
+		self.target = target
+		self.port = port
+
+def scan(sock):
+	s = socket.socket()
+	try:
+		s.connect((sock.target, sock.port))
+		print "\tListening on port: %d"%sock.port
+	except socket.error:
+		pass
+	finally:
+		s.close()
+
+print "Scanning for open ports on: %s"%alvo
+
+for porta in range(int(portas.split("-")[0]),int(portas.split("-")[1]) + 1):
+	scan(Sock(alvo,porta))
